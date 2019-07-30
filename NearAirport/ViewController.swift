40f98ae80cde7c3a61a0199c75730e1b29ae2262
Nandana <nandana.behera@cognizant.com>
2019-07-30 12:04:48 +0530

    Initial Commit

---
+++ NearAirport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearAirport
+//
+//  Created by Behera, Nandana (Cognizant) on 30/07/2019.
+//  Copyright © 2019 Behera, Nandana (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
